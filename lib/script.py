ee1029ae6a3e9217c37dff3b4dff6b3137b0c661
Rendon Elsesser <rendonelsesser@gmail.com>
2019-02-26 16:14:14 -0500

    working on game logic

--- lib/script.py
+++ lib/script.py
@@ -1,15 +1,32 @@
 from capitals import states
-from random import shuffle
+import random 
 
 print("Hi welcome to the best States game ever!!!")
 
-def shuffle_state()
-    shuffle(state)
+def game_start():
+    round = 0
+    random.shuffle(states)
+
+
+# for "everything" in this array do "this" every object is a score
 
 for score in states:
-    state["correct"] = 0
-    state["incorrect"] = 0
+    score['correct'] = 0
+    score['incorrect'] = 0
+    score['counter'] = 0
+    # print(score['round'])
 
+
+for question in states:
+    answer = input("What is the capital of " + question["name"] + "? ")
+    if answer == question["capital"]:
+        question['correct'] += 1
+        print("yes!")
+    else:
+        question['incorrect'] += 1
+        print("wrong!")
+    print("{} right, {} incorrect".format(question['correct'],question["incorrect"]))
+    question['counter'] += 1
 
 
 print(states)
