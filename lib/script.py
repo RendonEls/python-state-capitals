ff2b577e9539c31887d0971bc70cdc3adbb497c3
Rendon Elsesser <rendonelsesser@gmail.com>
2019-02-26 16:22:58 -0500

    finished logic, game works

--- lib/script.py
+++ lib/script.py
@@ -3,10 +3,6 @@ import random
 
 print("Hi welcome to the best States game ever!!!")
 
-def game_start():
-    round = 0
-    random.shuffle(states)
-
 
 # for "everything" in this array do "this" every object is a score
 
@@ -17,16 +13,26 @@ for score in states:
     # print(score['round'])
 
 
-for question in states:
-    answer = input("What is the capital of " + question["name"] + "? ")
-    if answer == question["capital"]:
-        question['correct'] += 1
-        print("yes!")
-    else:
-        question['incorrect'] += 1
-        print("wrong!")
-    print("{} right, {} incorrect".format(question['correct'],question["incorrect"]))
-    question['counter'] += 1
+def game_start():
+    random.shuffle(states)
+    round = 0
+    for question in states:
+        answer = input("What is the capital of " + question["name"] + "? ")
+        if answer == question["capital"]:
+            question['correct'] += 1
+            print("yes!")
+        else:
+            question['incorrect'] += 1
+            print("wrong!")
+        print("{} right, {} incorrect".format(question['correct'],question["incorrect"]))
+        question['counter'] += 1
+        round += 1
+        print(round)
+        if round == 50:
+            replay = input("Would you like to play agian? y/n ")
+            if replay == "y":
+                game_start()
+            else: return
 
-
-print(states)
+game_start()
+# print(states)
